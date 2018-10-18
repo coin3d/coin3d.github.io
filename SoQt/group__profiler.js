7184fce2418f50697ef7ad696d79446f53755d26
Volker Enderlein <volkerenderlein@hotmail.com>
2018-10-18 19:52:12 +0200

    Updated SoQt documentation.

---
+++ SoQt/group__profiler.js
@@ -0,0 +1,5 @@
+var group__profiler =
+[
+    [ "COIN_PROFILER", "C:/Data/Volker/Bitbucket/installed/share/doc/Coin/html/group__profiler.html#gaa961552d7a272cffa96986008e4559ef", null ],
+    [ "COIN_PROFILER_OVERLAY", "C:/Data/Volker/Bitbucket/installed/share/doc/Coin/html/group__profiler.html#ga7a452448717da283f227a6e0372d98e1", null ]
+];
