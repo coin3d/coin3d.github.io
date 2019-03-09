dbb99459746dae6d97ff43494e8ed182cb537786
Volker Enderlein <volkerenderlein@hotmail.com>
2019-03-09 14:44:41 +0100

    Added SoXt documentation.

---
+++ SoXt/navtreedata.js
@@ -0,0 +1,33 @@
+var NAVTREE =
+[
+  [ "SoXt", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"SoXtBasic_8h_source.html",
+"classSoXtFullViewer.html#a33952b22e0386794e1fc4e08b794c2e3",
+"classSoXtPlaneViewer.html#aca63283e093c98db1dc30209dcdeb532",
+"classSoXtViewer.html#aa42582272bc27f8963722031b8304434"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
