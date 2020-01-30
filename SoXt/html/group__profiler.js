8caea21b078108aa170da0683635a6f317abe21b
Volker Enderlein <volkerenderlein@hotmail.com>
2020-01-30 22:34:03 +0100

    Correct cross references in SoXt documentation.

---
+++ SoXt/html/group__profiler.js
@@ -0,0 +1,5 @@
+var group__profiler =
+[
+    [ "COIN_PROFILER", "../../Coin/html/group__profiler.html#gaa961552d7a272cffa96986008e4559ef", null ],
+    [ "COIN_PROFILER_OVERLAY", "../../Coin/html/group__profiler.html#ga7a452448717da283f227a6e0372d98e1", null ]
+];
--- SoXt/html/navtreedata.js
+++ SoXt/html/navtreedata.js
@@ -26,7 +26,8 @@ var NAVTREEINDEX =
 "SoXtBasic_8h_source.html",
 "classSoXtFullViewer.html#a33952b22e0386794e1fc4e08b794c2e3",
 "classSoXtPlaneViewer.html#aca63283e093c98db1dc30209dcdeb532",
-"classSoXtViewer.html#aa42582272bc27f8963722031b8304434"
+"classSoXtViewer.html#aa42582272bc27f8963722031b8304434",
+"group__envvars.html#gaa24faeaeabed94bc9ff732669b879a8a"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
